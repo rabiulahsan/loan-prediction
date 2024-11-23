c0a5b395e534048b2541dcc8b54c61205bccb183
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-11-23 14:55:39 +0600

    create load object function

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import dill
+import pickle
+
+import numpy as np 
+import pandas as pd 
+
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix
+from src.exception import CustomException
+from src.logger import logging
+
+
+
+
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
